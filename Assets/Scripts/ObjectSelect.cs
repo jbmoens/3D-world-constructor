f38972d1a952ff82f18a4a24e5a4b638eeee6e06
jbmoens <JB@LONLTM2864.home>
2017-02-17 18:51:17 +0100

    First commit from laptop
    Created Scene_Laptop, with specific ObjectSelect script

--- Assets/Scripts/ObjectSelect.cs
+++ Assets/Scripts/ObjectSelect.cs
@@ -2,27 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectSelect : MonoBehaviour {
+public class ObjectSelect : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown (0))
-		{
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
 			RaycastHit hit;
 			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out hit))
-			{
-				if (hit.collider.tag == "Platform")
-				{
-					Debug.Log ("You hit a platform!");
-					hit.transform.gameObject.GetComponent<Renderer>().material.color = Color.green;
-				}
-			}
+			if (Physics.Raycast (ray, out hit) && hit.collider.tag == "Platform" && hit.normal == Vector3.up)
+				hit.transform.gameObject.GetComponent<Renderer> ().material.color = Color.green;
 		}
 	}
 }
